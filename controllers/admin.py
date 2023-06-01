47c53a7b85e442446db962d3b4f6ff2ff5095a3d
Jianxff <3109441270@qq.com>
2023-06-01 23:11:04 +0800

    [U] update structure

--- controllers/admin.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-admin = Blueprint("admin", __name__)
-
-@admin.route("/query/state")
-def query_state():
-    raise NotImplementedError
-
-# TODO: Add more routes here
--- controllers/user.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-user = Blueprint("user", __name__)
-
-@user.route("/alter/mode")
-def alter_mode():
-    raise NotImplementedError
-
-# TODO: Add more routes here
