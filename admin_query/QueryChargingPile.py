052fd2af2910bfcdb36119c9e1dae9b98e87b454
Mars Cain <71871209+Go2Heart@users.noreply.github.com>
2023-06-01 23:06:25 +0800

    Update QueryChargingPile.py

--- admin_query/QueryChargingPile.py
+++ admin_query/QueryChargingPile.py
@@ -1,9 +1,8 @@
-# 各充电桩的当前状态信息（是否正常工作、系统启动后累计充电次数、充电总时长、充电总电量
+
 
 def get_charging_piles_status(charging_piles):
     status = []
     for pile in charging_piles:
-        # 模拟获取充电桩状态信息的过程，这里简单地返回一些随机数据
         working_status = pile.working_status
         total_charges = pile.total_charges
         total_time = pile.charge_time
