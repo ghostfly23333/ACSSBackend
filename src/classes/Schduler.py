1ccc885de35a6d24e8fc49702a4ddc14e206cb1f
ghostfly23333 <ghostfly233@outlook.com>
2023-06-02 17:58:04 +0800

    Update Schedulere.py

--- src/classes/Schduler.py
+++ src/classes/Schduler.py
@@ -1,5 +1,9 @@
-from sortedcontainers import SortedSet
+# WARNING: NOT TESTED YET
+
 from src.classes.ChargingPile import ChargingPile
+from src.classes.ChargingRequest import ChargingRequest
+from src.analyzer.charging_request import get_charging_request
+from src.classes.WaitingArea import waiting_area
 class Scheduler:
     fast_piles_num: int
     slow_piles_num: int
@@ -11,10 +15,10 @@ class Scheduler:
         for pile in charging_piles:
             if pile.pile_type == PileType.Fast:
                 self.fast_piles_num += 1
-                self.piles[0].append(pile)
+                self.piles[0].append(pile.pile_id)
             else:
                 self.slow_piles_num += 1
-                self.piles[1].append(pile)
+                self.piles[1].append(pile.pile_id)
                 
             
 
@@ -24,7 +28,8 @@ class FIFOScheduler(Scheduler):
     def add_query(self, charge_mode, car_id) -> (bool, int):
         minimum_time = 1e9
         minimum_index = -1
-        for pile in self.piles[charge_mode]:
+        for pile_index in self.piles[charge_mode]:
+            pile = charging_piles[pile_index]
             if pile.status == PileState.Idle or pile.status == PileState.Working:
                 pile_time = 0
                 if len(pile.cars_queue) < 2:
@@ -38,23 +43,31 @@ class FIFOScheduler(Scheduler):
         else:
             request = get_charging_request(car_id)
             info = ChargingInfo(car_id, 0, request.amount, 0)
-            self.piles[charge_mode][minimum_index].cars_queue.append(info)
+            charging_piles[minimum_index].cars_queue.append(info)
             return True, minimum_index
     def shutdown_pile(self, charge_mode, pile_id):
         info_list = list()
-        for info in self.piles[charge_mode][pile_id].cars_queue:
+        for info in charging_piles[pile_id].cars_queue:
+            # TODO: generate bill
             info_list.append((get_charging_request(info.car_id).queue_num, ChargingInfo(info.car_id, 0, info.all_amount - info.changed_amount, 0)))
-        self.piles[charge_mode][pile_id].cars_queue.clear()
-        for pile in self.piles[charge_mode]:
+        charging_piles[pile_id].cars_queue.clear()
+        for pile_index in self.piles[charge_mode]:
+            pile = charging_piles[pile_index]
             if pile.pile_id == pile_id:
                 continue
             if len(pile.cars_queue) > 1:
                 info = pile.cars_queue[1]
                 info_list.append((get_charging_request(info.car_id).queue_num, info))
             pile.cars_queue.pop(1)
-        info_list.sort(key = lambda x:x[0])
+        info_list.sort(key = lambda x:x[0][1:])
         for info in info_list:
             inserted = self.add_query(charge_mode, info.car_id)
-            if inserted == False:
-                # TODO: insert the query back to waiting area
+            if inserted.first == False:
+                request = get_charging_request(info.car_id)
+                request.set_pile_id(0)
+                waiting_area.enter(info.car_id, charge_mode)
                 break
+            else:
+                request.set_pile_id(inserted.second)
+
+            
