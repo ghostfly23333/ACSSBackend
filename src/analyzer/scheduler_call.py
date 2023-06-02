d6c3272bf3a78294d4b348fc7d7cf4dffd4b9c2d
Jianxff <3109441270@qq.com>
2023-06-02 17:52:44 +0800

    update scheduler call

--- src/analyzer/scheduler_call.py
+++ src/analyzer/scheduler_call.py
@@ -1,13 +1,27 @@
-from src.classes.ChargingRequest import ChargingRequest
+from classes.ChargingRequest import ChargingRequest
 from analyzer.charging_request import get_charging_request
-
+from classes.ChargingPile import is_vacant
+import threading
+lock = threading.Lock()
 requests = list(str)
 
 def add_request(r: str, force: bool = False) :
-    if not force and len(requests) >= 6:
-      return False
-    requests.append(r) 
-    return True
+    with lock:
+      if not force and len(requests) >= 6:
+        return False
+      requests.append(r)
+      threading.Thread(target=process_new_request(r)).start()
+      return True
+
+
+def process_new_request(r:str):
+    req = get_charging_request(r)
+    if req is None:
+        return
+    if is_vacant(req.mode):
+        # scheduler
+        pass
+    
 
 def get_request(mode: int):
     min = 0
@@ -23,17 +37,17 @@ def get_request(mode: int):
 
 def call_scheduler():
     # scheduler
-    
+
     pass
 
-
+# for use in end of charging
 def notify_available(mode: int):
     r = get_request(mode)
     if r is not None:
       # scheduler
       pass
 
-
+# for use in end of scheduling
 def notify_scheduled(r:str):
     # scheduler
     del requests[r]
