3d9da24136a876c2cb300eeb73f9901b82763ec0
Kqramazov <yurisvarozhich@gmail.com>
2023-06-02 10:31:35 +0800

    class_bill

---
+++ src/classes/Bill.py
@@ -0,0 +1,12 @@
+class Bill:
+    bill_id:str
+    date:str
+    pile:int
+    car:str
+    mode:int
+    amount:float
+    duration:float
+    start_time:str
+    end_time:str
+    service_cost:float
+    total:float
